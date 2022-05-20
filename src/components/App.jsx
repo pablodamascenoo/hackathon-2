f89768e0e597dccc26075a47e1d628e82689a86b
LuigiVanin <luisfvaninmartins@gmail.com>
2022-05-20 18:50:19 -0300

    feat: login stilos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
-import FirstPage from "../pages/FistPage";
+import Login from "../pages/Login";
+import GlobalStyles from "../assets/global_styles/Global.style";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Reset from "../assets/global_styles/Reset";
 
 export default function App() {
     return (
         <BrowserRouter>
             <Reset />
+            <GlobalStyles />
             <Routes>
-                <Route path="/" element={<FirstPage />} />
+                <Route path="/" element={<Login />} />
             </Routes>
         </BrowserRouter>
     );
