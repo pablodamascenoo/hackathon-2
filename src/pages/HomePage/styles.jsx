d6aa04d725b78e6f8bb7d842836138c460f82dc3
JV-Albuquerque-S <alb.albqrq@gmail.com>
2022-05-20 21:42:27 -0300

    estilizando mais a homepage

--- src/pages/HomePage/styles.jsx
+++ src/pages/HomePage/styles.jsx
@@ -11,8 +11,32 @@ export const Container = styled.main`
 `
 
 export const Section = styled.div`
-    width: 100%;
+    width: 80%;
     display: flex;
-    flex-direction: column;
     justify-content: center;
+    margin-top: 30px;
+
+    article {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        justify-content: space-between;
+        padding: 30px;
+        font-family: 'Poppins', sans-serif;
+    }
+
+    h1 {
+        font-size: 20px;
+        color: #3c147c;
+    }
+
+    h2 {
+        font-size: 16px;
+    }
+
+    img {
+        width: 150px;
+        height: 150px;
+        border-radius: 300px;
+    }
 `
